56a39a4b7deaf490b1d5a8316745eae4d2bc81b2
gyy00 <giyiany@gmail.com>
2023-12-04 22:41:36 +0000

    Add files via upload

--- currency.py
+++ currency.py
@@ -14,7 +14,6 @@ def getHist(today):
             'EUR': []}
     
     today = date.today()
-    yesterday = today - timedelta(days = 1)
 
     
     for i in range(7):
@@ -22,14 +21,14 @@ def getHist(today):
             day = str(today - timedelta(days = 1))
         else:
             day = str(today - timedelta(days = 5*i))
-        hist['Date'].append(day)
+        hist['Date'].insert(0, day)
 
         #Data from https://app.freecurrencyapi.com/
         data_url = 'https://api.currencyapi.com/v3/historical?apikey=fca_live_p0TopWEyEj0qqAgOYzV4U2hsHwryEwAYwDmrVEO1&currencies=USD%2CEUR&base_currency=GBP&date='+day
         response = requests.get(data_url)
         rate = json.loads(response.text)
-        hist['USD'].append(rate['data']['USD']['value'])
-        hist['EUR'].append(rate['data']['EUR']['value'])
+        hist['USD'].insert(0,rate['data']['USD']['value'])
+        hist['EUR'].insert(0, rate['data']['EUR']['value'])
     return hist
 
 
@@ -38,9 +37,10 @@ def main():
     data = getHist(today)
 
     #Example data
-##    data = {'Date': ['2023-12-03', '2023-11-29', '2023-11-24', '2023-11-19', '2023-11-14', '2023-11-09', '2023-11-04'],
-##    'USD': [1.2715041358, 1.2693896922, 1.2604457551, 1.2451747705, 1.249234694, 1.2216871693, 1.2375163123],
-##    'EUR': [1.1674444161, 1.1566933975, 1.1520601554, 1.1417755913, 1.1479968824, 1.1451242008, 1.1527342115]}
+##    data = {'Date': ['2023-11-04', '2023-11-09', '2023-11-14', '2023-11-19', '2023-11-24', '2023-11-29', '2023-12-03'],
+##    'USD': [1.2375163123, 1.2216871693, 1.249234694, 1.2451747705, 1.2604457551, 1.2693896922, 1.2715041358],
+##    'EUR': [1.1527342115, 1.1451242008, 1.1479968824, 1.1417755913, 1.1520601554, 1.1566933975, 1.1674444161]}
+
 
     #Create pandas dataframe
     df = pd.DataFrame.from_dict(data)
@@ -53,8 +53,8 @@ def main():
     fig, ax = plt.subplots()
 
     #Plot both data sets on the same axes
-    ax.plot(x, y1, color='blue', label='USD')
-    ax.plot(x, y2, color='red', label='EUR')
+    ax.plot(x, y1, color='red', label='USD')
+    ax.plot(x, y2, color='blue', label='EUR')
 
     #Adjust layout so that the date fits within the window
     plt.subplots_adjust(bottom=0.25)
